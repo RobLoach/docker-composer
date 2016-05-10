6c4365e30f816b16bd05dee7a9e89c745d538f46
William Hearn <sylus1984@gmail.com>
2016-05-10 18:35:59 -0400

    Update variable naming to camelCase.

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 #
 # Usage: ./update.sh x.y.z
 #
-# This script runs to create a Dockerfile for a new Composer version.
-# If you specify a partial version, like '1' or '1.0', it will determine the most recent sub version like 1.0.1.
-set -e
+# This script runs to create a Dockerfile for a new Drush version.
+# If you specify a partial version, like '8' or '8.0', it will determine the most recent sub version like 8.0.1.
+set -eo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
@@ -15,44 +15,45 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
-function write_files {
-	local full_version=$1
+function writeFiles {
+	local fullVersion=$1
 	local variant=$2
 
-	short_version=$(echo $full_version | sed -r -e 's/^([0-9]+.[0-9]+.[0-9]+)/\1/')
+	shortVersion=$(echo $fullVersion | sed -r -e 's/^([0-9]+.[0-9]+.[0-9]+)/\1/')
 	if [[ -z $variant ]]; then
-		target_dir="$short_version"
+		targetDir="$shortVersion"
 		template=Dockerfile.template
 	else
-		target_dir="$short_version/$variant"
+		targetDir="$shortVersion/$variant"
 		template=Dockerfile-$variant.template
 	fi
 
-	mkdir -p "$target_dir"
-	cp $template "$target_dir/Dockerfile"
+	mkdir -p "$targetDir"
+	cp $template "$targetDir/Dockerfile"
 	if [[ -f docker-entrypoint.sh ]]; then
-		cp -r docker-entrypoint.sh "$target_dir"
+		cp -r docker-entrypoint.sh "$targetDir"
 	fi
-	sed -r -i -e 's/^(ENV COMPOSER_VERSION) .*/\1 '"$full_version"'/' "$target_dir/Dockerfile"
+	sed -r -i -e 's/^(ENV COMPOSER_VERSION) .*/\1 '"$fullVersion"'/' "$targetDir/Dockerfile"
 }
 
 tags="$(git ls-remote --tags https://github.com/composer/composer.git | cut -d/ -f3 | cut -d^ -f1 | cut -dv -f2 | sort -rV)"
 
 for version in "${versions[@]}"; do
-	possible_versions="$(echo "$tags" | grep "^$version" )"
-	if release_versions="$(echo "$possible_versions" | grep -vEm1 '\-alpha|\-beta')"; then
-		full_version="$release_versions"
+	possibleVersions="$(echo "$tags" | grep "^$version" )"
+	if releaseVersions="$(echo "$possibleVersions" | grep -ivEm1 'milestone|-alpha|-beta|-rc')"; then
+		fullVersion="$releaseVersions"
 	else
-		full_version="$(echo "$possible_versions" | head -n1)"
+		fullVersion="$(echo "$possibleVersions" | head -n1)"
 	fi
 
-	if [[ -z $full_version ]]; then
+	if [[ -z $fullVersion ]]; then
 		echo "Cannot find version: $version"
 		exit 1
 	fi
+
 	(
 		set -x
-		write_files $full_version
-		write_files $full_version 'alpine'
+		writeFiles $fullVersion
+		writeFiles $fullVersion 'alpine'
 	)
 done
